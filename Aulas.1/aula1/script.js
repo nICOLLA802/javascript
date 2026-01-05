848255617fcb76f2efe68bb655c7ee92d2fac6df
Santos <nicollasbezerra05@gmail.com>
2026-01-04 23:17:49 -0300

    .

---
+++ Aulas.1/aula1/script.js
@@ -0,0 +1,8 @@
+const name = ["nicollas", "bezerra", "santos"];
+name.push("pedro"); //adiciona ao final
+name.unshift("joao"); //adiciona no inicio
+name.shift(); //remove um item ao final
+name.sort(); // ordena os items
+console.log(name.indexOf("santos")); //Diz onde esta determinado item
+console.log(name.length); //conta quantos itens tem em uma lista
+console.log(name); //imprime em tela
